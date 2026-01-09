96f3497ae7722ac97a90966a1fdb7e73ac23f332
Max Lovell <max_lovell@hotmail.co.uk>
2026-01-09 16:04:33 +0000

    Init a participant data class

---
+++ Assets/Scripts/UI/ParticipantData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+[Serializable]
+public class ParticipantData
+{
+    public string ParticipantID;
+    public string Handedness;
+    public string Gender;
+    public int Age;
+    public string Ethnicity;
+    public int YearsEducation;
+    public string AlcoholFreq;
+    public string CannabisFreq;
+
+    // Condition Data
+    public string ConditionName; // e.g., "Researcher Strokes"
+}
